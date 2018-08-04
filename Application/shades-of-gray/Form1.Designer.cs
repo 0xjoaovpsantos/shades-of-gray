36f967e014c12c2bb767cddb1f79513d2d4d5f2a
João Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2018-08-04 08:35:02 -0300

    Adding shades-of-gray 1.0.0

---
+++ Application/shades-of-gray/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace shades_of_gray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img1 = new System.Windows.Forms.PictureBox();
+            this.img2 = new System.Windows.Forms.PictureBox();
+            this.btnCarregarImagem = new System.Windows.Forms.Button();
+            this.btnTransformar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img1
+            // 
+            this.img1.Location = new System.Drawing.Point(12, 12);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(471, 354);
+            this.img1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img1.TabIndex = 0;
+            this.img1.TabStop = false;
+            // 
+            // img2
+            // 
+            this.img2.Location = new System.Drawing.Point(513, 12);
+            this.img2.Name = "img2";
+            this.img2.Size = new System.Drawing.Size(466, 354);
+            this.img2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img2.TabIndex = 1;
+            this.img2.TabStop = false;
+            // 
+            // btnCarregarImagem
+            // 
+            this.btnCarregarImagem.Location = new System.Drawing.Point(372, 388);
+            this.btnCarregarImagem.Name = "btnCarregarImagem";
+            this.btnCarregarImagem.Size = new System.Drawing.Size(111, 51);
+            this.btnCarregarImagem.TabIndex = 2;
+            this.btnCarregarImagem.Text = "Carregar imagem";
+            this.btnCarregarImagem.UseVisualStyleBackColor = true;
+            this.btnCarregarImagem.Click += new System.EventHandler(this.btnCarregarImagem_Click);
+            // 
+            // btnTransformar
+            // 
+            this.btnTransformar.Location = new System.Drawing.Point(513, 388);
+            this.btnTransformar.Name = "btnTransformar";
+            this.btnTransformar.Size = new System.Drawing.Size(111, 51);
+            this.btnTransformar.TabIndex = 3;
+            this.btnTransformar.Text = "Transformar";
+            this.btnTransformar.UseVisualStyleBackColor = true;
+            this.btnTransformar.Click += new System.EventHandler(this.btnTransformar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 469);
+            this.Controls.Add(this.btnTransformar);
+            this.Controls.Add(this.btnCarregarImagem);
+            this.Controls.Add(this.img2);
+            this.Controls.Add(this.img1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img1;
+        private System.Windows.Forms.PictureBox img2;
+        private System.Windows.Forms.Button btnCarregarImagem;
+        private System.Windows.Forms.Button btnTransformar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Application/shades-of-gray/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shades_of_gray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCarregarImagem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            img1.Load(openFileDialog1.FileName);
+        }
+
+        private void btnTransformar_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            int coluna = img.Width;
+            int linha = img.Height;
+            Bitmap imgnova = new Bitmap(coluna, linha);
+            Color cor = new Color();
+            for (int i = 0; i <= coluna - 1; i++)
+            {
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    double r = img.GetPixel(i, j).R * 0.30;
+                    double g = img.GetPixel(i, j).G * 0.59;
+                    double b = img.GetPixel(i, j).B * 0.11;
+                    int newColor = (int)(r + g + b) / 3;
+                    cor = Color.FromArgb(255, newColor, newColor, newColor);
+                    imgnova.SetPixel(i, j, cor);
+                }
+            }
+            imgnova.Save(Application.StartupPath + "\\nova-imagem.bmp");
+            img2.Load(Application.StartupPath + "\\nova-imagem.bmp");
+        }
+    }
+}
